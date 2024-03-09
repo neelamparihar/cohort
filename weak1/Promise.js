2f783e803d9b107892f3cb5b2d888b1ce922deab
Neelam <pariharneelam376@gmail.com>
2024-03-09 12:01:30 +0530

    practice

--- weak1/Promise.js
+++ weak1/Promise.js
@@ -1,1 +1,11 @@
 // What is promise :--  It is just a class that makes callbacks and async function slightly more readable.
+
+var d = new Promise(function (resolve) {
+  resolve("Heyy Neelam");
+});
+
+function callback() {
+  console.log(d);
+}
+
+d.then(callback);
